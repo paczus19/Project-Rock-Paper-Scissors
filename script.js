0941849c9f3a5b174dafe14ec0dd9b1b72914c18
Robert Pączek <paczus19@aol.com>
2024-03-12 21:15:21 +0100

    Add working game: Rock Paper Scissors, create function for Computer choice, Player selection and use loop for each game round

--- script.js
+++ script.js
@@ -3,46 +3,54 @@ const ComputerOptions = ["rock" , "paper" , "scissors"];
 let ComputerChoice = ComputerOptions[Math.floor(Math.random()*3)];
 return ComputerChoice; }
 
-let ComputerSelection = getComputerChoice();
+let PlayerScore = 0;
+let ComputerScore = 0; 
 
 function round() {
-    let PlayerSelection = prompt("Type your option: rock, paper or scissors");
-    let PlayerChoice = PlayerSelection.toLowerCase();
-    console.log("Player choice is: "+PlayerChoice);
+    let ComputerSelection = getComputerChoice();
+    let PlayerSelection = prompt("Type your option: rock, paper or scissors").toLowerCase();
+    
+    console.log("Player choice is: "+PlayerSelection);
     console.log("Computer choice is: "+ComputerSelection);
 
-    if(PlayerChoice == ComputerSelection) {
-        console.log("It's a draw!"); }
+    if(PlayerSelection == ComputerSelection) {
+        console.log("It's a draw!");
+        console.log("Player: "+ (PlayerScore+=0) +", Computer: "+ (ComputerScore+=0)); }
 
-    else if(PlayerChoice == "paper" && ComputerSelection == "rock") {
-        console.log("Player Wins!"); }
+    else if(PlayerSelection == "paper" && ComputerSelection == "rock") {
+        console.log("Player Wins!");
+        console.log("Player: "+ (PlayerScore+=1) +", Computer: "+(ComputerScore+=0)); }
 
-    else if(PlayerChoice == "paper" && ComputerSelection == "scissors") {
-        console.log("Computer Wins!"); }
+    else if(PlayerSelection == "rock" && ComputerSelection == "scissors") {
+        console.log("Player Wins!");
+        console.log("Player: "+ (PlayerScore+=1) +", Computer: "+(ComputerScore+=0)); }
 
-    else if(PlayerChoice == "rock" && ComputerSelection == "paper") {
-        console.log("Computer Wins!"); }
+    else if(PlayerSelection == "scissors" && ComputerSelection == "paper") {
+        console.log("Player Wins!");
+        console.log("Player: "+ (PlayerScore+=1) +", Computer: "+(ComputerScore+=0)); }
 
-    else if(PlayerChoice == "rock" && ComputerSelection == "scissors") {
-        console.log("Player Wins!"); }
+    else if(PlayerSelection == "paper" && ComputerSelection == "scissors") {
+        console.log("Computer Wins!");
+        console.log("Player: "+ (PlayerScore+=0) +", Computer: "+(ComputerScore+=1)); }
 
-    else if(PlayerChoice == "scissors" && ComputerSelection == "rock") {
-        console.log("Computer Wins!"); }
-    
-    else if(PlayerChoice == "scissors" && ComputerSelection == "paper") {
-        console.log("Player Wins!"); }
+    else if(PlayerSelection == "rock" && ComputerSelection == "paper") {
+        console.log("Computer Wins!"); 
+        console.log("Player: "+ (PlayerScore+=0) +", Computer: "+(ComputerScore+=1)); }
 
-    else { console.log("There's a mistake in Code"); }
-}
+    else if(PlayerSelection == "scissors" && ComputerSelection == "rock") {
+        console.log("Computer Wins!"); 
+        console.log("Player: "+ (PlayerScore+=0) +", Computer: "+(ComputerScore+=1)); }
+
+    else { console.log("Choose the right option"); } }
 
 function Game(){
-    for(let i=0; i<6; i++)
-    {
-        round();
-        console.log(" ");
-        console.log(" ");
-    }
-}
+    for(let i=0; i<Infinity; i++)
+    {  round();
+    console.log(" ");
+    if(PlayerScore == 5) {
+        console.log("Congrats, You WIN!") }
+    else if(ComputerScore == 5) {
+        console.log("Game over, Computer WINS!") }
+    else { console.log(" ") } } }
 
 Game();
-
